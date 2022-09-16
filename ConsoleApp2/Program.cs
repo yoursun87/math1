6be5b0d66acc38aa24b56b7d4a97c060500ad566
79277 <79277@LAPTOP-4JH0IHNA>
2022-09-16 15:51:16 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] a = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            a[i] = rnd.Next(0, 100);
+            Console.WriteLine("{0,2}", a[i]);
+
+        }
+        Console.WriteLine();
+        int min = a[0];
+        for (int i = 1; i < 10; i++) ;
+        min = Math.Min(min,a[0]);
+        Console.WriteLine(" Минимальное значение:" + min);
+        Console.ReadKey();
+    }
+
+        
+}
+
+
+
